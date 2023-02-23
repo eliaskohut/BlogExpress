34e90e4ac39dd2f3d709d3809385de48937a874b
eliaskohut <misty0mist@gmail.com>
2023-02-23 02:16:21 +0100

    init commit

--- server/routes/blogRoutes.js
+++ server/routes/blogRoutes.js
@@ -2,7 +2,40 @@ const express = require('express');
 const router = express.Router();
 const blogController = require('../controllers/blogController');
 
+const path = require('path');
+const multer = require('multer');
+const upload = multer({ dest: 'uploads/' });
+
+const Entrie = require('../models/Entrie')
+
+// serve files from the "public" directory
+router.use(express.static(path.join(__dirname, '../public')));
+
 //App Routes
 router.get('/', blogController.homepage);
+router.get('/createEntrie', blogController.createEntrie)
+router.post('/createEntrie', upload.single('file'), async (req, res) => {
+  const { name } = req.body;
+  const file = req.file;
 
-module.exports=router;
+  if (!file) {
+    return res.status(400).send('No file uploaded');
+  }
+
+  const type = file.mimetype.startsWith('audio') ? 'audio' : 'image';
+  const description = req.body.description || null;
+  const filename = req.body.filename || null;
+
+  const entrie = new Entrie({ name, type, description, filename });
+  try {
+    await entrie.save();
+    res.redirect('/');
+  } catch (err) {
+    console.error(err);
+    res.status(500).send('Server Error');
+  }
+});
+
+
+
+module.exports = router;
