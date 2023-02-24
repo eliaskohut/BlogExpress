86d8b300efff08a77f2176d5f0e36c043c4baab2
eliaskohut <misty0mist@gmail.com>
2023-02-24 17:12:53 +0100

    fcomm

--- app.js
+++ app.js
@@ -2,10 +2,24 @@ const express = require('express');
 const expressLayouts = require('express-ejs-layouts');
 const mongoose = require('mongoose');
 const app = express();
-const port = process.env.PORT || 4000;
+const port = process.env.PORT || 3000;
 
 require('dotenv').config();
 mongoose.set('strictQuery', false);
+
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+const session = require('express-session');
+
+app.use(session({
+  secret: 'your-secret-key',
+  resave: false,
+  saveUninitialized: false
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
 
 
 // Connect to database
